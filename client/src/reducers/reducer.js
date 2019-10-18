3536fd12e1fb4190f7b154b89e971486c9760d0f
Tony <tonyrkovar@gmail.com>
2019-10-18 10:24:08 -0500

    setting up redux and added private route component

---
+++ client/src/reducers/reducer.js
@@ -0,0 +1,11 @@
+
+const initialState = {
+    data: []
+}
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
